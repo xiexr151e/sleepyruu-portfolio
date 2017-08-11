d24b605c47bf4be8046a6d8c319d543520ed2c15
lu-ai <jarek13705@gmail.com>
2017-08-11 02:12:39 -0700

    First live!
    In a usable state. Need to finish portfolio, JP translation.

--- old/js/main.js
+++ old/js/main.js
@@ -1,63 +1,15 @@
-function voidWidth(lbar, slogo, sitemap) {
-    void lbar.offsetWidth;
-    void slogo.offsetWidth;
-    void sitemap.offsetWidth;
-}
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "Hello!" +
+    			"\nIf you're here, you're probably wondering who made this site." +
+    			"\nI am but a simple friend of Ruu's. Possibly her brother." + 
+    			"\nHave a nice day!"
+   				);
 
-/* Function to remove exit animations to reuse entrance */
-function removeFadeAnimation(lbar, slogo, sitemap) {
-    lbar.classList.remove("fade-out-left");
-    slogo.classList.remove("fade-out-right");
-    sitemap.classList.remove("fade-out-left");
-}
-
-/* Function to re-add the default entrance animations */
-function addDefEntrance(lbar, slogo, sitemap) {
-    lbar.classList.add("fade-in-left");
-    slogo.classList.add("fade-in-right");
-    sitemap.classList.add("tracking-in-expand");
-}
-
-$(document).ready(function(){
-
-    var lbar = document.getElementById("leftbar");
-    var slogo = document.getElementById("blogo");
-    var sitemap = document.getElementById("smap");
-
-    /* Animation to make the side elements smoothly fade out
-     * when the Twtich bar is clicked */ 
-    $('.hideall').click(function(){
-    	$('.fade-in-left').toggleClass('fade-out-left');
-    	$('.fade-in-right').toggleClass('fade-out-right');
-    	$('.tracking-in-expand').toggleClass('fade-out-left');
-
-        console.log('Selected');
-    });
-
-    /* Animation to smoothly return back to the main menu
-     * from the Twitch screen */
-    $('.return').click(function() {
-        /* Remove the fade animation, then forcibly reset the offsetWidth
-         * to reuse entrance animations */
-        removeFadeAnimation(lbar, slogo, sitemap);
-        voidWidth(lbar, slogo, sitemap);
-        addDefEntrance(lbar, slogo, sitemap);
-    });
-
-    /* Method to hide the right sidebar when a normal element is clicked */
-    $('.content').click(function() {
-        slogo.classList.add("fade-out-right");
-    });
-
-    /* Method to restore the right sidebar when returning to landing */
-    $('.landing').click(function() {
-
-        slogo.classList.remove("fade-out-right");
-        /* Reset offset width to reuse animations */
-        void slogo.offsetWidth;
-        slogo.classList.add("fade-in-right");
-    })
-
-
-
+    /* Modified modal code so clicking outside the modal closes it */
+    $('.modal-dialog').on('click tap', function(e){
+  		if ($(e.target).hasClass('modal-dialog')) {
+    		$('.modal').modal('hide');
+  		}
+	});
 });
